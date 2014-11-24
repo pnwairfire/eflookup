e7c4c7b71d8983b151e828373946611bd526f74f
Joel Dubowy <jdubowy@gmail.com>
2014-11-24 14:31:28 -0800

    Set up skeleton repo for fccs2ef package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+
+REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+
+setup(
+    name='fccs2ef',
+    version='0.1.0',
+    author='Joel Dubowy',
+    author_email='jdubowy@gmail.com',
+    packages=[
+        'fccs2ef',
+        'fccs2ef.bluesky'],
+    scripts=[],
+    url='git@bitbucket.org:fera/airfire-fccs2ef.git',
+    description='Package supporting lookup of emissions factors for FCCS fuel bed types.',
+    install_requires=REQUIREMENTS,
+)
