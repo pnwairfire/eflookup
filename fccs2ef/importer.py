759cae332a8255140272712fc8c14bc5502283a8
Joel Dubowy <jdubowy@gmail.com>
2014-11-26 12:52:26 -0800

    Refactor importer to encode urbanski groups as integers
    I now need to write code to import urbanski-to-EF mapping data,
    and then I need to update the loading code

--- fccs2ef/importer.py
+++ fccs2ef/importer.py
@@ -5,27 +5,78 @@ Note that this is module is only useful for a) creating the data
 files included in this module, or b) creating custom data files.
 """
 
+import abc
 import csv
 import logging
 
-class Fccs2UrbanskiImporter(object):
+class UrbanskiGroups(object):
+    """Enumeration representing Urbansnki cover type groups.
+
+    @note: Future versions of python have an Enum class build in, added by
+    https://www.python.org/dev/peps/pep-0435/.  It's not worth requiring the
+    backport (https://pypi.python.org/pypi/enum34) here, though.
+    """
+    BOREAL_FOREST = 1
+    BOREAL_RESIDUAL = 2
+    CWD_RESIDUAL = 3
+    GRASS = 4
+    NW_FOREST = 5
+    SE_FOREST = 6
+    SHRUB = 7
+    SW_FOREST = 8
+    WESTERN_FOREST = 9
+    TEMPERATE_RESIDUAL = 10
+
+class ImporterBase(object):
 
     def __init__(self, input_file_name):
         self._load(input_file_name)
+
+    def _str_to_group_id(self, val):
+        gid = self.GROUP_IDS.get(val.lower())
+        if not gid:
+            logging.error("Failed to look up urbanski group '%s'", val)
+        return gid
 
     def _load(self, input_file_name):
         self._mappings = []
         with open(input_file_name, 'rb') as csvfile:
             csvfile.next() # header
             for row in csv.reader(csvfile, delimiter=',', skipinitialspace=True):
-                #logging.debug(str(row))
-                self._mappings.append({
-                    'fccs_id': int(row[0]),
-                    'urbanski_flame_smold_wf': row[9],  # row 'J'
-                    'urbanski_residual': row[10],  # row 'K'
-                    'urbanski_duff': row[11],  # row 'L'
-                    'urbanski_flame_smold_rx': row[12]  # row 'M'
-                })
+                self._process_row(row)
+
+    @abc.abstractmethod
+    def _process_row(self, row):
+        pass
+
+    @abc.abstractmethod
+    def write(self, output_file_name):
+        pass
+
+class Fccs2UrbanskiImporter(ImporterBase):
+
+    # TODO: fill in missing ids
+    GROUP_IDS = {
+        'boreal forest':UrbanskiGroups.BOREAL_FOREST,
+        'boreal residual': UrbanskiGroups.BOREAL_RESIDUAL,
+        'cwd residual': UrbanskiGroups.CWD_RESIDUAL,
+        'grass': UrbanskiGroups.GRASS,
+        'nw forest': UrbanskiGroups.NW_FOREST,
+        'se forest': UrbanskiGroups.SE_FOREST,
+        'sw forest': UrbanskiGroups.SW_FOREST,
+        'shrub': UrbanskiGroups.SHRUB,
+        'temperate residual': UrbanskiGroups.TEMPERATE_RESIDUAL,
+        'western forest': UrbanskiGroups.WESTERN_FOREST
+    }
+
+    def _process_row(self, row):
+        self._mappings.append({
+            'fccs_id': int(row[0]),
+            'urbanski_flame_smold_wf': self._str_to_group_id(row[9]),  # row 'J'
+            'urbanski_residual': self._str_to_group_id(row[10]),  # row 'K'
+            'urbanski_duff': self._str_to_group_id(row[11]),  # row 'L'
+            'urbanski_flame_smold_rx': self._str_to_group_id(row[12])  # row 'M'
+        })
 
     def write(self, output_file_name):
         with open(output_file_name, 'wb') as csvfile:
@@ -33,11 +84,12 @@ class Fccs2UrbanskiImporter(object):
                 "urbanski_duff,urbanski_flame_smold_rx\n")
             for m in sorted(self._mappings, lambda a,b: cmp(a['fccs_id'], b['fccs_id'])):
                 #logging.debug(str(m))
-                csvfile.write(','.join([
-                    str(m['fccs_id']),
+                csvfile.write(','.join([str(e or '') for e in [
+                    m['fccs_id'],
                     m['urbanski_flame_smold_wf'],
                     m['urbanski_residual'],
                     m['urbanski_duff'],
                     m['urbanski_flame_smold_rx'],
                     '\n'
-                ]))
+                ]]))
+
