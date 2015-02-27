b2db0c3cdef20ed9a1ca3cbd0410652ae491c7ae
Joel Dubowy <jdubowy@gmail.com>
2015-02-27 15:01:08 -0800

    Change setup url to repo homepage

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     package_data={
         'fccs2ef': ['data/*.csv']
     },
-    url='git@github.com:pnwairfire/fccs2ef.git',
+    url='https://github.com/pnwairfire/fccs2ef/',
     description='Package supporting lookup of emissions factors by FCCS fuelbed or cover type.',
     install_requires=REQUIREMENTS,
 )
