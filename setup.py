8024f3d4b951dd943d90e0260747673c699aaada
Joel Dubowy <jdubowy@gmail.com>
2015-03-24 16:47:48 -0700

    Refactor dependency parsing and setting to try to get depencies installed correctly when using pip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 # results in the folloing error on Heroku:
 #    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
-git_url_matcher = re.compile('^git\+.+/([^/.]+)(\.git)?@v?([0-9.]+)$')
+git_url_matcher = re.compile('^git\+(.+)/([^/.]+)(\.git)?@v?([0-9.]+)$')
 def parse_requirements(dep_links, req_file_name):
     with open(req_file_name) as f:
         reqs = []
@@ -13,8 +13,9 @@ def parse_requirements(dep_links, req_file_name):
             m = git_url_matcher.match(r)
             if m:
                 # TODO: figure out how dep link should be formatted
-                dep_links.append(r + '#egg=%s-%s' % (m.group(1), m.group(3)))
-                reqs.append("%s==%s" % (m.group(1), m.group(3)))
+                dep_links.append('%s/%s@v%s#egg=%s-%s' % (
+                    m.group(1), m.group(2), m.group(4), m.group(2), m.group(4)))
+                reqs.append("%s==%s" % (m.group(2), m.group(4)))
             else:
                 reqs.append(r)
         return reqs
