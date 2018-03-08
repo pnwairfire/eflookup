3bfa49baf3e605a5ed444f9b5868636e58983168
Joel Dubowy <jdubowy@gmail.com>
2018-03-07 17:05:52 -0800

    Move reusable const to constants module

--- eflookup/fccs2ef/importer.py
+++ eflookup/fccs2ef/importer.py
@@ -12,6 +12,8 @@ import csv
 import logging
 import re
 
+from ..constants import CONSUME_FUEL_CATEGORY_TRANSLATIONS
+
 __all__ = [
     'Fccs2CoverTypeImporter',
     'CoverType2EfGroupImporter',
@@ -136,44 +138,7 @@ class CatPhase2EFGroupImporter(ImporterBase):
         "Generic Assignment": "generic_assignment"
     }
 
-    CATEGORY_TRANSLATIONS = {
-        "c_over_crown": "canopy:overstory",
-        "c_mid_crown": "canopy:midstory",
-        "c_under_crown": "canopy:understory",
-        "c_snagc1f_crown": "canopy:snags class 1 foliage",
-        "c_snagc1f_wood": "canopy:snags class 1 wood",
-        "c_snagc1": "canopy:snags class 1 no foliage",
-        "c_snagc2": "canopy:snags class 2",
-        "c_snagc3": "canopy:snags class 3",
-        "c_ladder": "canopy:ladder fuels",
-        "c_shrub_1live": "shrub:primary live",
-        "c_shrub_1dead": "shrub:primary dead",
-        "c_shrub_2live": "shrub:secondary live",
-        "c_shrub_2dead": "shrub:secondary dead",
-        "c_herb_1live": "nonwoody:primary live",
-        "c_herb_1dead": "nonwoody:primary dead",
-        "c_herb_2live": "nonwoody:secondary live",
-        "c_herb_2dead": "nonwoody:secondary dead",
-        "c_wood_1hr": "woody fuels:1-hr fuels",
-        "c_wood_10hr": "woody fuels:10-hr fuels",
-        "c_wood_100hr": "woody fuels:100-hr fuels",
-        "c_wood_s1000hr": "woody fuels:1000-hr fuels sound",
-        "c_wood_r1000hr": "woody fuels:1000-hr fuels rotten",
-        "c_wood_r10k": "woody fuels:10000-hr fuels sound",
-        "c_wood_s10k": "woody fuels:10000-hr fuels rotten",
-        "c_wood_+s10k": "woody fuels:10k+-hr fuels sound",
-        "c_wood_+r10k": "woody fuels:10k+-hr fuels rotten",
-        "c_stump_sound": "woody fuels:stumps sound",
-        "c_stump_rotten": "woody fuels:stumps rotten",
-        "c_stump_lightered": "woody fuels:stumps lightered",
-        "c_litter": "litter-lichen-moss:litter",
-        "c_lichen": "litter-lichen-moss:lichen",
-        "c_moss": "litter-lichen-moss:moss",
-        "c_upperduff": "ground fuels:duff upper",
-        "c_lowerduff": "ground fuels:duff lower",
-        "c_ba": "ground fuels:basal accumulations",
-        "c_sm": "ground fuels:squirrel middens"
-    }
+
 
     SECOND_ROW_HEADER_PROCESSOR = re.compile(':.*$')
 
@@ -258,7 +223,7 @@ class CatPhase2EFGroupImporter(ImporterBase):
 
         if self._headers[idx] == 'consume_output_variable':
             k = self.CONSUME_CATEGORY_PROCESSOR.sub('', val).lower()
-            return self.CATEGORY_TRANSLATIONS[k]
+            return CONSUME_FUEL_CATEGORY_TRANSLATIONS[k]
 
         return val.lower()
 
