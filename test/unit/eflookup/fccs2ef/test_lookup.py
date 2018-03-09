d853dbd2d7df58a2a5d9b15187b477c591bd9767
Joel Dubowy <jdubowy@gmail.com>
2018-03-09 14:48:16 -0800

    Continued WIP on fccs2ef.lookup unit tests

--- test/unit/eflookup/fccs2ef/test_lookup.py
+++ test/unit/eflookup/fccs2ef/test_lookup.py
@@ -26,9 +26,9 @@ CAT_PHASE_2_EF_GROUP = {
     "24-26": {
         "canopy":{
             "ladder fuels":{
-                "flaming":{"NH3":"24","NOx":"24","PM25":"24","SO2":"24",},
-                "residual":{"NH3":None,"NOx":"25","PM25":None,"SO2":None,},
-                "smoldering":{"NH3":"24","NOx":"24","PM25":"24","SO2":"24",},
+                "flaming":{"NH3":"24","PM25":"24"},
+                "residual":{"NH3":None,"PM25":None},
+                "smoldering":{"NH3":"24","PM25":"24"},
             }
         }
     }
@@ -57,11 +57,11 @@ EF_GROUP_2_EF = {
         "PM2.5":"12.51"
     },
     "8": {
-        "CH4":"3",
-        "CO":"2",
-        "CO2":"1",
-        "PM10":"10.1",
-        "PM2.5":"12.51"
+        "CH4":"33",
+        "CO":"22",
+        "CO2":"11",
+        "PM10":"110.1",
+        "PM2.5":"122.51"
     },
     "24":{
         "CH4":"1.98",
@@ -79,6 +79,7 @@ EF_GROUP_2_EF = {
     },
 }
 
+
 ##
 ## Base Classes
 ##
@@ -91,10 +92,12 @@ class TestFccs2EfAndCovertype2EF(object):
         covertype2efgroup.COVERTYPE_2_EF_GROUP = COVERTYPE_2_EF_GROUP
         catphase2efgroup.CAT_PHASE_2_EF_GROUP = CAT_PHASE_2_EF_GROUP
 
-        self.fccs2ef_213_rx = Fccs2Ef('213', True)
-        self.fccs2ef_213_wf = Fccs2Ef('213', False)
-        self.ct2ef_213_rx = CoverType2Ef('1', True)
-        self.ct2ef_213_wf = CoverType2Ef('10', False)
+        self.lookups = {
+            "fccs2ef_213_rx": Fccs2Ef('213', True),
+            "fccs2ef_213_wf": Fccs2Ef('213', False),
+            "ct2ef_1_rx": CoverType2Ef('1', True),
+            "ct2ef_10_wf": CoverType2Ef('10', False)
+        }
 
     def test_invalid_fccs_and_covertype_ids(self):
         # fccs id 1 isn't in ou
@@ -108,59 +111,88 @@ class TestFccs2EfAndCovertype2EF(object):
             CoverType2Ef("2", False)
 
     def test_invalid_get(self):
+        for l in self.lookups.values():
+            # nothing specified
+            with raises(LookupError) as e_info:
+                l.get()
+            # only phase is specified
+            with raises(LookupError) as e_info:
+                l.get(phase='phase')
+            with raises(LookupError) as e_info:
+                l.get(phase='phase', fuel_category='cat')
+            with raises(LookupError) as e_info:
+                l.get(phase='phase', fuel_category='cat',
+                    fuel_sub_category='subcat')
 
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_rx.get()
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_wf.get()
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_rx.get()
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_wf.get()
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_rx.get(phase='phase')
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_wf.get(phase='phase')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_rx.get(phase='phase')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_wf.get(phase='phase')
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_rx.get(phase='phase', fuel_category='cat')
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_wf.get(phase='phase', fuel_category='cat')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_rx.get(phase='phase', fuel_category='cat')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_wf.get(phase='phase', fuel_category='cat')
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_rx.get(phase='phase', fuel_category='cat',
-                fuel_sub_category='subcat')
-        with raises(LookupError) as e_info:
-            self.fccs2ef_213_wf.get(phase='phase', fuel_category='cat',
-                fuel_sub_category='subcat')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_rx.get(phase='phase', fuel_category='cat',
-                fuel_sub_category='subcat')
-        with raises(LookupError) as e_info:
-            self.ct2ef_213_wf.get(phase='phase', fuel_category='cat',
-                fuel_sub_category='subcat')
+    def test_get_nonexisting_phase(self):
+        for l in self.lookups.values():
+            # invalid phase
+            assert None == l.get(phase='foo', fuel_category='canopy', fuel_sub_category='ladder fuels', species='NH3')
+            assert None == l.get(phase='foo', fuel_category='canopy', fuel_sub_category='overstory', species='NH3')
 
-    def test_nonexisting_get(self):
-        assert None == self.fccs2ef_213_rx.get(phase='phase',
-            fuel_category='cat', fuel_sub_category='subcat', species='species')
-        assert None == self.fccs2ef_213_wf.get(phase='phase',
-            fuel_category='cat', fuel_sub_category='subcat', species='species')
-        assert None == self.ct2ef_213_rx.get(phase='phase',
-            fuel_category='cat', fuel_sub_category='subcat', species='species')
-        assert None == self.ct2ef_213_wf.get(phase='phase',
-            fuel_category='cat', fuel_sub_category='subcat', species='species')
+    def test_get_nonexisting_species(self):
+        for l in self.lookups.values():
+            # invalid species
+            assert None == l.get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='foo')
+            assert None == l.get(phase='phase', fuel_category='cat',
+                fuel_sub_category='subcat', species='species')
 
-    def test_get(self):
-        # No override specified
-        # 'None' override
-        # float value override
-        pass
+    def test_get_existing_no_override(self):
+        ## neither 'nonwoody' nor 'primary live' is in the overrides
+        for l in self.lookups.values():
+            # NH3 isn't specified
+            assert None == l.get(phase='flaming', fuel_category='nonwoody', fuel_sub_category='primary live', species='NH3')
+        # rx is EFG 6
+        assert 8.51 == self.lookups['fccs2ef_213_rx'].get(phase='flaming', fuel_category='nonwoody', fuel_sub_category='primary live', species='PM2.5')
+        assert 8.51 == self.lookups['ct2ef_1_rx'].get(phase='flaming', fuel_category='nonwoody', fuel_sub_category='primary live', species='PM2.5')
+        # ef is EFG 5
+        assert 9.51 == self.lookups['fccs2ef_213_wf'].get(phase='flaming', fuel_category='nonwoody', fuel_sub_category='primary live', species='PM2.5')
+        assert 9.51 == self.lookups['ct2ef_1_wf'].get(phase='flaming', fuel_category='nonwoody', fuel_sub_category='primary live', species='PM2.5')
+
+        ## 'canopy' is in overrides, but 'overstory' isn't
+        for l in self.lookups.values():
+            # NH3 isn't specified
+            assert None == l.get(phase='flaming', fuel_category='canopy', fuel_sub_category='overstory', species='NH3')
+        # rx is EFG 6
+        assert 8.51 == self.lookups['fccs2ef_213_rx'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='overstory', species='PM2.5')
+        assert 8.51 == self.lookups['ct2ef_1_rx'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='overstory', species='PM2.5')
+        # ef is EFG 5
+        assert 9.51 == self.lookups['fccs2ef_213_wf'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='overstory', species='PM2.5')
+        assert 9.51 == self.lookups['ct2ef_1_wf'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='overstory', species='PM2.5')
+
+        ## 'ladder fuels' is in overrides, but woody fuels isn't
+        for l in self.lookups.values():
+            assert None == l.get(phase='flaming', fuel_category='woody fuels', fuel_sub_category='ladder fuels', species='NH3') # woody fuels > ladder fuels would never really happen
+        # rx is EFG 6
+        assert 8.51 == self.lookups['fccs2ef_213_rx'].get(phase='flaming', fuel_category='woody fuels', fuel_sub_category='ladder fuels', species='PM2.5') # woody fuels > ladder fuels would never really happen
+        assert 8.51 == self.lookups['ct2ef_1_rx'].get(phase='flaming', fuel_category='woody fuels', fuel_sub_category='ladder fuels', species='PM2.5') # woody fuels > ladder fuels would never really happen
+        # wf is EFG 5
+        assert 9.51 == self.lookups['fccs2ef_213_wf'].get(phase='flaming', fuel_category='woody fuels', fuel_sub_category='ladder fuels', species='PM2.5') # woody fuels > ladder fuels would never really happen
+        assert 9.51 == self.lookups['ct2ef_1_wf'].get(phase='flaming', fuel_category='woody fuels', fuel_sub_category='ladder fuels', species='PM2.5') # woody fuels > ladder fuels would never really happen
+
+        ## 'canopy' > 'ladder fuels' is in overrides, but C02 isn't
+        for l in self.lookups.values():
+            assert None == l.get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='C11H22')
+        # rx is EFG 6
+        assert 1705 == self.lookups['fccs2ef_213_rx'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='CO2')
+        assert 1705 == self.lookups['ct2ef_1_rx'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='CO2')
+        # wf is EFG 5
+        assert 170 == self.lookups['fccs2ef_213_wf'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='CO2')
+        assert 170 == self.lookups['ct2ef_1_wf'].get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='CO2')
+
+        ## Other
+        for l in self.lookups.values():
+            assert None == l.get(phase='residual', fuel_category='canopy', fuel_sub_category='ladder fuels', species='CO2') # no residual emissions
+            assert 1 == l.get(phase='residual', fuel_category='ground fuels', fuel_sub_category='duff upper', species='CO2') #CWD - EFG 7
+            assert 11 == l.get(phase='residual', fuel_category='ground fuels', fuel_sub_category='duff upper', species='CO2') #duff - EFG 8
+
+    def test_get_existing_w_None_override(self):
+        for l in self.lookups.values():
+            assert None == l.get(phase='residual', fuel_category='canopy', fuel_sub_category='ladder fuels', species='NH3')
+
+    def test_get_existing_w_float_override(self):
+        for l in self.lookups.values():
+            assert 9.89 == l.get(phase='flaming', fuel_category='canopy', fuel_sub_category='ladder fuels', species='PM2.5')
 
     def test_species(self):
         pass
